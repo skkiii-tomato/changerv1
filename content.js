74d6664173fb522252ded24cfddc468fa1998f3f
skkiii-tomato <spousti2031@francisparker.org>
2025-11-13 11:03:26 -0800

    Create content.js

---
+++ content.js
@@ -0,0 +1,6 @@
+title = document.querySelector("title");
+title.innerHTML = "Britannica School";
+
+link = document.querySelector("link[rel~='icon']");
+
+link.href = "https://cdn.britannica.com/mendel-resources/3-167/images/EBLogo.jpg?v=3.167.20";
