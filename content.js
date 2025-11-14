ed381f72ac5297949203b059ef25309718e7260d
skkiii-tomato <spousti2031@francisparker.org>
2025-11-14 15:19:12 -0800

    Update content.js

--- content.js
+++ content.js
@@ -5,4 +5,6 @@ link = document.querySelector("link[rel~='icon']");
 
 link.href = "https://cdn.britannica.com/mendel-resources/3-167/images/EBLogo.jpg?v=3.167.20";
 
-document.querySelector("head").appendChild("<img width = "100%" height = "700px" src="https://static01.nyt.com/images/2019/02/11/world/11iran-revolution2/merlin_150228654_8dd1b219-f3fa-459a-b9bf-7ee10f3aae00-articleLarge.jpg?quality=75&auto=webp&disable=upscale">);
+let image = '<img width = "100%" height = "700px" src="https://static01.nyt.com/images/2019/02/11/world/11iran-revolution2/merlin_150228654_8dd1b219-f3fa-459a-b9bf-7ee10f3aae00-articleLarge.jpg?quality=75&auto=webp&disable=upscale">';
+
+document.querySelector("head").appendChild(image);
